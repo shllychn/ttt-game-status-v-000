b19237429310307658387a3998b1c00bb807a32d
Shelly Chen <shellyjychen@gmail.com>
2018-06-15 04:15:17 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -21,7 +21,7 @@ def won?(board)
   WIN_COMBINATIONS.each do |win_combo|
     if (board[win_combo[0]]) == "X" && (board[win_combo[1]]) == "X" && (board[win_combo[2]]) == "X"
       return win_combo
-    else (board[win_combo[0]]) == "O" && (board[win_combo[1]]) == "O" && (board[win_combo[2]]) == "O"
+    elsif (board[win_combo[0]]) == "O" && (board[win_combo[1]]) == "O" && (board[win_combo[2]]) == "O"
       return win_combo
     end
   end
